e9a5198eba004e43178c5a5727ea553a6e71d459
mugabe-rob <mugaberobert222004644@gmail.com>
2025-08-05 11:01:02 +0200

    added the contributions for a gruop of people

---
+++ src/routes/contribution.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const contributionController = require('../controllers/contribution.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
+
+router.use(authMiddleware.verifyToken);
+
+router.post('/', contributionController.createContribution);
+router.get('/', contributionController.getAllContributions);
+router.get('/group/:groupId', contributionController.getGroupContributions);
+
+module.exports = router;
